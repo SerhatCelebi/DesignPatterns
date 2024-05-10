9574f46d6cc918e954aa5425b284222a2b68d7bb
SerhatCelebi <serhat6701@gmail.com>
2024-05-10 12:46:34 +0300

    Add project files.

---
+++ Deneme/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+public abstract class DenemeBase(string name2)
+    {
+        public abstract string name { get; set; }
+        public void Doit(string name2)
+        {
+            Console.WriteLine("Name");
+        }
+}
+}
